3324abd6cec1b0c174aaef05402018aec4c7df45
subbat9710 <teela.subba1991@gmail.com>
2017-09-30 19:40:37 -0400

    Primitive data type

---
+++ PrimitiveDataType.java
@@ -0,0 +1,46 @@
+public class PrimitiveDataType {
+	
+	public static void main(String args[]) {
+
+//for short, its range is -32768 to 32767
+//size: 2 bytes
+	/*	short num;		
+	*	num = 32767;
+	*/
+		
+//for long, its range is -9,223,372,036,854,775,808 to 9,223,372,036,854,775,807
+//size: 8 byte, Default value: 0
+		/*long num;		
+		num = -12332252626L;
+		*/
+		
+//for double, sufficient for holdong 15 decimal digits
+//Size: 8 bytes
+		
+		/*double num;
+		num = -42937737.9d;
+		*/
+		
+//for float, sufficient for holding 6 to 7 decimal digits
+//Size: 4 bytes
+		
+		/*float num;
+		num = 19.98f;
+		
+      	System.out.println(num);
+      	*/
+		
+//for boolean, it holds for true or false
+		
+		/*boolean b = false;
+		System.out.println(b);
+		*/
+		
+//Char holds for characters
+//Size: 2 bytes
+		
+		char ch = 'Z';
+		System.out.println(ch);
+	}
+
+}

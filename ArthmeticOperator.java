7de9e1afab6ab8d64c48ca1c8241191afca3995e
subbat9710 <teela.subba1991@gmail.com>
2017-09-30 20:10:18 -0400

    Arthmetic operator

---
+++ ArthmeticOperator.java
@@ -0,0 +1,17 @@
+//basic arthmetic operators are: +, -, *, /, %
+//Note: Modulo(%) operator returns remainder, for exampel 4%2 would return 0
+
+public class ArthmeticOperator {
+
+	public static void main(String args[]) {
+
+		int num1 = 100;
+		int num2 = 20;
+
+		System.out.println("num1 + num2: " + (num1 + num2) );
+		System.out.println("num1 - num2: " + (num1 - num2) );
+		System.out.println("num1 * num2: " + (num1 * num2) );
+		System.out.println("num1 / num2: " + (num1 / num2) );
+		System.out.println("num1 % num2: " + (num1 % num2) );
+	}
+}
